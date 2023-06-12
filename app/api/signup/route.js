4d24039584681db781070fa58d98a7c469c94ada
kailash8799 <kailashrajput8799@gmail.com>
2023-06-12 08:12:57 +0530

    added nodemailer

--- app/api/signup/route.js
+++ app/api/signup/route.js
@@ -1,7 +1,8 @@
-import {  NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import dbConnect from "@/database/_dbconnect";
 import User from "@/models/User";
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
+import nodemailer from "nodemailer";
 
 export async function POST(req) {
   await dbConnect();
@@ -9,21 +10,64 @@ export async function POST(req) {
   if (body?.email && body?.password) {
     const u = await User.findOne({ email: body?.email });
     if (u) {
-      return NextResponse.json({
-        success: false,
-        message: "User already exits",
-      },{status:400});
+      return NextResponse.json(
+        {
+          success: false,
+          message: "User already exits",
+        },
+        { status: 400 }
+      );
     }
-    const hashPassword = await bcrypt.hash(body?.password,12);
-    await User.create({name:body?.name,email:body?.email,password:hashPassword})
-    return NextResponse.json({
-      success: true,
-      message: "Account has been created successfully",
-    },{status:200});
+    const hashPassword = await bcrypt.hash(body?.password, 12);
+    const cu = await User.create({
+      name: body?.name,
+      email: body?.email,
+      password: hashPassword,
+    });
+    if (cu) {
+      let email = `We have sent you this email in response to your request to reset your password on Codeswear.com
+
+      To reset your password, please follow the link below:
+      http://localhost:3000/forget?token=${cu?._id}
+
+      We recommend that you keep your password secure and not share it with anyone.If you feel your password has been compromised, you can change it by going to your My Account Page and update your password `;
+      // --------------------------------
+      var transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: process.env.EMAIL,
+          pass: process.env.PASSWORD_EMAIL,
+        },
+      });
+      var mailOptions = {
+        from: process.env.EMAIL,
+        to: cu?.email,
+        subject: "Verify User",
+        text: email,
+      };
+
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Email sent");
+        }
+      });
+    }
+    return NextResponse.json(
+      {
+        success: true,
+        message: "Account has been created successfully",
+      },
+      { status: 200 }
+    );
   } else {
-    return NextResponse.json({
-      success: false,
-      message: "Please check the email and password",
-    },{status:500});
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Please check the email and password",
+      },
+      { status: 500 }
+    );
   }
 }
